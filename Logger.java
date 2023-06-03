dcb0f6984e0fac17eea503ecaf82c810b5d1458b
Andi Moran Flores <amoflo22@unm.edu>
2023-06-02 20:16:45 -0600

    pushing the logger file

---
+++ Logger.java
@@ -0,0 +1,11 @@
+package week05;
+
+/*
+ * This part of the code dictates how the actions or what it can or cant do
+ * 
+ */
+public interface Logger {
+	public void log(String log);
+
+	public void error(String error);
+}
